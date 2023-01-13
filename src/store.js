375d4d6bed82c8250f0500fc16acbbca4356e878
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-01-13 07:59:29 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -3,18 +3,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers'
 
-console.log("Aqui empiezo")
-
 const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
     rootReducer,
     initialState,
-    console.log("Aqui me rompo primero"),
     applyMiddleware(...middleware)
     //composeWithDevTools(applyMiddleware(...middleware))
 );
-console.log("Aqui me rompo segundo")
+
 export default store; 
-console.log("Aqui me rompo tercero")
